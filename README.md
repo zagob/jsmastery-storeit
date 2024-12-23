696b66c42da825a5cec6b2f1831a1f5b69df0e7d
zagob <matheusbestana@gmail.com>
2024-12-23 17:59:14 -0300

    search

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-// 4:43:41
+// 4:52:41
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
 
 ## Getting Started
--- components/search.tsx
+++ components/search.tsx
@@ -3,31 +3,49 @@
 import Image from "next/image";
 import { Input } from "./ui/input";
 import { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { getFiles } from "@/lib/actions/file.actions";
 import { Models } from "node-appwrite";
+import Thumbnail from "./thumbnail";
+import FormattedDateTime from "./formatted-date-time";
 
 const Search = () => {
   const [query, setQuery] = useState("");
-  const searchParams = useSearchParams()
-  const searchQuery = searchParams.get('query') || ""
-  const [results, setResults] = useState<Models.Document[]>([])
-  const [open, setOpen] = useState(false)
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("query") || "";
+  const [results, setResults] = useState<Models.Document[]>([]);
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const path = usePathname();
 
   useEffect(() => {
     const fetchFiles = async () => {
-      const files = await getFiles({ types, searchText: query, sort, limit })
-      setResults(files.documents)
-      setOpen(true)
-    }
-  }, [])
+      if (!query) {
+        setResults([]);
+        setOpen(false);
+        return router.push(path.replace(searchParams.toString(), ""));
+      }
+      const files = await getFiles({ searchText: query });
+      setResults(files.documents);
+      setOpen(true);
+    };
+
+    fetchFiles();
+  }, [query]);
 
   useEffect(() => {
-    if(!searchQuery){
-      setQuery('')
+    if (!searchQuery) {
+      setQuery("");
     }
+  }, [searchQuery]);
 
-  }, [searchQuery])
+  const handleClickItem = (file: Models.Document) => {
+    setOpen(false);
+    setResults([]);
+    router.push(
+      `/${file.type === "video" || file.type === "audio" ? "media" : file.type + "s"}?query=${query}`
+    );
+  };
 
   return (
     <div className="search">
@@ -44,6 +62,39 @@ const Search = () => {
           className="search-input"
           onChange={(e) => setQuery(e.target.value)}
         />
+
+        {open && (
+          <ul className="search-result">
+            {results.length > 0 ? (
+              results.map((file) => (
+                <li
+                  key={file.$id}
+                  className="flex items-center justify-between"
+                  onClick={() => handleClickItem(file)}
+                >
+                  <div className="flex cursor-pointer items-center gap-4">
+                    <Thumbnail
+                      type={file.type}
+                      extension={file.extension}
+                      url={file.url}
+                      className="size-9 min-w-9"
+                    />
+                    <p className="subtitle-2 line-clamp-1 text-light-100">
+                      {file.name}
+                    </p>
+                  </div>
+
+                  <FormattedDateTime
+                    date={file.$createdAt}
+                    className="caption line-clamp-1 text-light-200"
+                  />
+                </li>
+              ))
+            ) : (
+              <p className="empty-result">No files found</p>
+            )}
+          </ul>
+        )}
       </div>
     </div>
   );
