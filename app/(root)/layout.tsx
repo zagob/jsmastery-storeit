8683d8da0281d7d494d1a3a0b75cf087c28be744
zagob <matheusbestana@gmail.com>
2024-12-24 15:13:05 -0300

    adding force dynamic

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,6 +6,8 @@ import { getCurrentUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
 import React from "react";
 
+export const dynamic = "force-dynamic";
+
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
 
